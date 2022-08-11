33fecb3b70e38a8220a636a1c20879de069ebb06
paulolemosfn <paulolemosfn@gmail.com>
2022-08-10 22:14:12 -0300

    fix: remove codes of all tables

---
+++ src/system/database/migrations/1660179971066-RemoveEagerTrueInTheTables.ts
@@ -0,0 +1,16 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class RemoveEagerTrueInTheTables1660179971066 implements MigrationInterface {
+    name = 'RemoveEagerTrueInTheTables1660179971066'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "pets_entity" DROP COLUMN "pet_code"`);
+        await queryRunner.query(`ALTER TABLE "owners" DROP COLUMN "code"`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "owners" ADD "code" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "pets_entity" ADD "pet_code" character varying NOT NULL`);
+    }
+
+}
