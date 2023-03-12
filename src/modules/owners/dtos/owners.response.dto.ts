ff7ec942f16ef27c420529f39f1e62fb855b9f6c
paulolemosfn <paulolemosfn@gmail.com>
2023-03-12 16:42:02 -0300

    refactor: refactor DTOs of the owners

--- src/modules/owners/dtos/owners.response.dto.ts
+++ src/modules/owners/dtos/owners.response.dto.ts
@@ -3,20 +3,14 @@ import { BaseResponseDTO } from '../../../common/dtos/base.response.dto';
 
 export class OwnersResponseDTO extends BaseResponseDTO {
   @ApiProperty({
-    type: 'uuid',
-    example: 'fb5c9676-4e28-4daa-8efa-375512451f8f',
-  })
-  readonly id: string;
-
-  @ApiProperty({
     type: 'string',
     example: 'owner name',
   })
-  readonly owner_name: string;
+  owner_name: string;
 
   @ApiProperty({
-    type: 'string',
-    example: 'DN01',
+    type: 'uuid',
+    example: 'fb5c9676-4e28-4daa-8efa-375512451f8f',
   })
-  readonly code: string;
+  user_id: string;
 }
---
+++ src/modules/owners/interfaces/owners-response.interface.ts
@@ -0,0 +1,11 @@
+export default interface OwnersInterface {
+  id: string;
+  created_by_name: string;
+  created_by_email: string;
+  updated_by_name: string;
+  updated_by_email: string;
+  created_at: Date;
+  owner_name: string;
+  pet_id: string;
+  user_id: string;
+}
