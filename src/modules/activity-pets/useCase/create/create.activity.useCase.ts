61a7c9b68452566dbf41430f1a86d67a71fe63b6
paulolemosfn <paulolemosfn@gmail.com>
2024-02-06 00:12:56 -0300

    refactor: create use case

--- src/modules/activity-pets/useCase/create/create.activity.useCase.ts
+++ src/modules/activity-pets/useCase/create/create.activity.useCase.ts
@@ -3,33 +3,18 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { format } from 'date-fns';
 import { buildCreateWithUser } from '../../../../common/builders/user-data-general.builders';
 import { DefaultHeadersInterface } from '../../../../common/interfaces/default-headers.interface';
-import { OwnersRepository } from '../../../owners/repositories/owners.repository';
 import { GetOwnerByIdUseCase } from '../../../owners/useCase/getById/getById.owners.useCase';
-import { PetsRepository } from '../../../pets/repositories/pets.repository';
 import { GetPetByIdUseCase } from '../../../pets/useCase/getById/getById.pets.useCase';
-import { UsersRepository } from '../../../users/repositories/users.repository';
-import { GetUsersByIdUseCase } from '../../../users/useCase/getById/getById.users.useCase';
 import { ActivityEntity } from '../../entities/activity.entity';
 import { ActivityRepository } from '../../repositories/activity.repository';
 import { CreateActivityDTO } from './create.activity.dto';
+import { formatDate } from '../../../../common/utils/date';
 
 @Injectable()
 export class CreateActivityUseCase {
   constructor(
     @InjectRepository(ActivityRepository)
     private repository: ActivityRepository,
-
-    @InjectRepository(PetsRepository)
-    private petsRepository: PetsRepository,
-
-    @InjectRepository(OwnersRepository)
-    private ownerRepository: OwnersRepository,
-
-    @InjectRepository(UsersRepository)
-    private usersRepository: UsersRepository,
-
-    @Inject(GetUsersByIdUseCase)
-    private getUserById: GetUsersByIdUseCase,
 
     @Inject(GetOwnerByIdUseCase)
     private getOwnerById: GetOwnerByIdUseCase,
@@ -46,31 +31,17 @@ export class CreateActivityUseCase {
 
     const data: Partial<ActivityEntity> = createActivity;
 
-    // const userFound = await this.getUserById.execute(user_id, defaultHeaders);
+    await this.getOwnerById.execute(data.owner_id, defaultHeaders);
 
-    const ownerFound = await this.getOwnerById.execute(
-      data.owner_id,
-      defaultHeaders,
-    );
-
-    const petFound = await this.getPetById.execute(data.pet_id, defaultHeaders);
-
-    data.pets = petFound;
-
-    if (!data.created_by) {
-      data.created_by = ownerFound.owner_name;
-    }
-
-    if (!data.activity_date) {
-      data.activity_date = format(new Date(), 'dd-MM-yyyy');
-    }
-
-    data.activity_hour = format(new Date(), 'HH:mm');
+    await this.getPetById.execute(data.pet_id, defaultHeaders);
 
     const createData = {
       ...data,
       user_id,
-    };
+      activity_hour: format(new Date(), 'HH:mm'),
+      activity_date: formatDate(new Date()),
+      created_by: defaultHeaders.username,
+    } as ActivityEntity;
 
     const activityData = buildCreateWithUser(createData, defaultHeaders);
 
